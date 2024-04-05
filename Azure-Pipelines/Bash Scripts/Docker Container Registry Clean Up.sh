938b444e55b11b495a771aa521fb143a8bf99533
Azure_Pipeline <85315025+Duuuuuuuuuude@users.noreply.github.com>
2024-04-05 22:00:11 +0000

    Updated Docker Container Registry Clean Up.sh

--- Azure-Pipelines/Bash Scripts/Docker Container Registry Clean Up.sh
+++ Azure-Pipelines/Bash Scripts/Docker Container Registry Clean Up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://laimis.medium.com/net-web-apps-on-digitalocean-85f0c3acd57f
+# Inspired by: https://laimis.medium.com/net-web-apps-on-digitalocean-85f0c3acd57f
 
 min_to_keep=10
 delete_days_old=3
@@ -24,7 +24,7 @@ run_garbage_collection() {
   echo ""$containerRegistryAPIBaseURL"/garbage-collection"
   echo "Error: HTTP response $http_status"
   echo "Response body: $json_response"
-  
+
   # Checks the HTTP status error codes
   if [ $http_status -ne 201 ]; then
     echo "Error: HTTP response $http_status"
@@ -34,26 +34,27 @@ run_garbage_collection() {
 }
 
 
-# Gets all tags in repository
+# Gets all Container Registry Repository Manifest in repository
 response=$(curl --silent \
                 --write-out "HTTPSTATUS:%{http_code}" \
                 -X GET \
                 -H "Content-Type: application/json" \
                 -H "Authorization: Bearer "$bearerTokenContainerRegistry"" \
-                ""$containerRegistryAPIBaseURL"/repositories/"$repositoryNameURLEncoded"/tags?per_page=200")
+                ""$containerRegistryAPIBaseURL"/repositories/"$repositoryNameURLEncoded"/digests?per_page=200")
 
 # Separates the JSON response from the HTTP status
 http_status=$(echo $response | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
 json_response=$(echo $response | sed -e 's/HTTPSTATUS\:.*//g')
 
-# Checks the HTTP status code and exits if the HTTP request failed. Also prints out the json body, if it failed and message if there was nothing to delete
-if [ $http_status -eq 404 ]; then
-    echo "Error: HTTP response $http_status"
-    if [[ "$verbose" == "yes" ]]; then
-      echo "Nothing to delete"
-    fi
-    exit 0
-elif [ $http_status -ne 200 ]; then
+# Checks the HTTP status code and exits if the HTTP request failed.
+#if [ $http_status -eq 404 ]; then
+#    echo "Error: HTTP response $http_status"
+#    if [[ "$verbose" == "yes" ]]; then
+#      echo "Nothing to delete"
+#    fi
+#    exit 0
+#el
+if [ $http_status -ne 200 ]; then
     echo "Error: HTTP response $http_status"
     echo "Response body: $json_response"
     exit 1
@@ -64,7 +65,7 @@ fi
 
 # Filters by delete_days_old and min_to_keep
 date_boundary=$(date -d-"$delete_days_old days" +%s)
-deletable_tags=$(echo $json_response | jq ".tags[$min_to_keep:] | .[] | select ( .updated_at | fromdateiso8601 < $date_boundary) | .tag " -r | tr '\n' ' ')
+deletable_digests=$(echo $json_response | jq ".manifests[$min_to_keep:] | .[] | select ( .updated_at | fromdateiso8601 < $date_boundary) | .digest " -r | tr '\n' ' ')
 
 # Check the exit status of jq for errors
 if [ $? -ne 0 ]; then
@@ -72,7 +73,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-[[ -z "$deletable_tags" ]] && {
+[[ -z "$deletable_digests" ]] && {
   [[ "$verbose" == "yes" ]] && echo "Nothing to delete"
   run_garbage_collection
   exit 0
@@ -82,18 +83,18 @@ fi
   echo "DELETING images in the container registry, that are more than $delete_days_old days"
   echo "While KEEPING at least $min_to_keep of the newest images in the container registry"
   echo ""
-  echo "Deleteable tags: $deletable_tags"
+  echo "Deleteable tags: $deletable_digests"
   echo ""
 }
 
-# Tags being deleted on the images in container registry.
-for tag in $deletable_tags; do
+# digests being deleted in the container registry.
+for digest in $deletable_digests; do
   response=$(curl --silent \
                   --write-out "HTTPSTATUS:%{http_code}" \
                   -X DELETE \
                   -H "Content-Type: application/json" \
                   -H "Authorization: Bearer "$bearerTokenContainerRegistry"" \
-                  ""$containerRegistryAPIBaseURL"/repositories/"$repositoryNameURLEncoded"/tags/$tag")
+                  ""$containerRegistryAPIBaseURL"/repositories/"$repositoryNameURLEncoded"/digests/$digest")
 
   # Separate the JSON response from the HTTP status
   http_status=$(echo $response | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
